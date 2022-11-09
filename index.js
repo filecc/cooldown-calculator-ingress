57fcfc814b88b92259821c33f9090e681e3e1d02
ynad <ynad92@gmail.com>
2022-11-09 16:57:01 +0100

    test event

--- index.js
+++ index.js
@@ -20,8 +20,8 @@ let eventName = "Back to 90s (of cooldown)"
 let coolDownEvent_friend = 90;
 let coolDownEvent_enemy = 150;
 let hacksEvent = 40;
-let event_start = "2022-11-09T08:01:00";
-let event_end = "2022-11-11T18:01:00";
+let event_start = "2022-11-09T08:30:00";
+let event_end = "2022-11-11T18:30:00";
 let eventLink = "https://ingress.com/news/";
 
 
